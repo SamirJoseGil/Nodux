96167d676c19402a80df38739cb312c739f7d859
Juan Pablo de Jesús Avendaño Bustamante <jpavendanb@eafit.edu.co>
2025-10-17 14:37:26 -0500

    feat(mentors, core): added Mentors endpoint with create and update capabilities and generated a mentor action called hours (it allows to register mentor attendance)

--- backend/apps/mentors/views.py
+++ backend/apps/mentors/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework import status
+from rest_framework.response import Response
+from .serializers import MentorSerializer
+from .models import Mentor, MentorAttendance
+from .serializers import MentorAttendanceSerializer
 
-# Create your views here.
+
+class MentorViewSet(viewsets.ModelViewSet):
+    queryset = Mentor.objects.all()
+    serializer_class = MentorSerializer
+
+    @action(detail=True, methods=["get", "post"])
+    def hours(self, request, pk=None):
+        """
+        Allows registering hours for a mentor.
+        Validations:
+
+        The 'hours' field is required.
+
+        Must be a positive integer.
+
+        Prevents daily duplicates per mentor.
+        """
+
+        if request.method == "GET":
+            registeredAttendance = MentorAttendance.objects.all()
+            serializer = MentorAttendanceSerializer(registeredAttendance, many=True)
+            return Response(serializer.data)
+        elif request.method == "POST":
+            mentor = self.get_object()
+            user = request.user
+            hours = request.data.get("hours")
+
+            data = {
+                "mentor": mentor.id,
+                "registered_by": user.id,
+                "hours": hours,
+            }
+
+            serializer = MentorAttendanceSerializer(data=data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save(registered_by=user)
+
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
