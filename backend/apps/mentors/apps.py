e786be88f4760a7c3fa7937294f3d6d42378b9e0
AmazingJuan <jpavendanb@eafit.edu.co>
2025-12-10 01:44:24 +0000

    deployment fixes

--- backend/apps/mentors/apps.py
+++ backend/apps/mentors/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class MentorsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.mentors'
+
+    def ready(self):
+        from .events import periodic
